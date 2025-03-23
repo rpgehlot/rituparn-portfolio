9ec5ce03b651ab9f8c629021582f48c1d6e602f1
Rituparn Gehlot <rituparn@Rituparns-Laptop.local>
2025-03-23 13:16:19 +0530

    added grid back

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
-// import Grid from "@/components/Grid";
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/floating-navbar";
@@ -14,7 +14,7 @@ export default function Home() {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems}  />
         <Hero />
-        {/* <Grid /> */}
+        <Grid />
         <RecentProjects />
         <Clients />
         <Experience />
