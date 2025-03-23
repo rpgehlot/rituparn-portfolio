c306bda6313d67e064e394e0c9e7770ae17b7300
Rituparn Gehlot <rituparn@Rituparns-Laptop.local>
2025-03-23 11:32:21 +0530

    Commenting document usage to verify working

--- components/ui/gradientAnimation.tsx
+++ components/ui/gradientAnimation.tsx
@@ -40,6 +40,7 @@ const BackgroundGradientAnimation = ({
   const [tgX, setTgX] = useState(0);
   const [tgY, setTgY] = useState(0);
   useEffect(() => {
+    /*
     document.body.style.setProperty(
       "--gradient-background-start",
       gradientBackgroundStart
@@ -56,6 +57,7 @@ const BackgroundGradientAnimation = ({
     document.body.style.setProperty("--pointer-color", pointerColor);
     document.body.style.setProperty("--size", size);
     document.body.style.setProperty("--blending-value", blendingValue);
+    */
   }, []);
 
   useEffect(() => {
