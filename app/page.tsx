2be7d134cc16e59502754a0035328ab7f89c540a
Rituparn Gehlot <rituparn@Rituparns-Laptop.local>
2025-03-23 13:10:38 +0530

    test with only Grid left

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,19 @@ import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 // import Grid from "@/components/Grid";
-// import Hero from "@/components/Hero";
-// import RecentProjects from "@/components/RecentProjects";
-// import { FloatingNav } from "@/components/ui/floating-navbar";
-// import { navItems } from "@/data";
+import Hero from "@/components/Hero";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
-        {/* <FloatingNav navItems={navItems}  />
+        <FloatingNav navItems={navItems}  />
         <Hero />
-        <Grid />
-        <RecentProjects /> */}
+        {/* <Grid /> */}
+        <RecentProjects />
         <Clients />
         <Experience />
         <Approach />
